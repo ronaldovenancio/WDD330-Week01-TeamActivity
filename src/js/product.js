bb6c10910827a12d310aab81e020f9c0105ad9fe
Ronaldo Venâncio da Silva <95707584+ronaldovenanciorvs@users.noreply.github.com>
2024-09-25 17:07:08 -0300

    upload product.js and vite.config.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,32 @@
 import { getParam } from "./utils.mjs";
-import { setLocalStorage } from "./utils.mjs";
-import ProductData  from "./ProductData.mjs";
+//import { setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
-console.log(dataSource.findProductById(productId));
+//console.log(dataSource.findProductById(productId));
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
+/*
 function addProductToCart(product) {
   localStorage.setItem("so-cart", product);
 
   setLocalStorage("so-cart", product);
   
-}
+}  */
+
 // add to cart button event handler
+/*
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
-}
+}   */
 
 // add listener to Add to Cart button
+/*
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+*/
