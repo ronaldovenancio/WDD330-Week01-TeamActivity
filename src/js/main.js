7249e82d0659385c71a6ab1f62d04bae3da0e992
Ronaldo Venâncio da Silva <95707584+ronaldovenanciorvs@users.noreply.github.com>
2024-09-27 12:37:56 -0300

    upload main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
+
+listing.init();
+
+
+/*
 import ProductData from "./ProductDetails.mjs";
 import ProductListing from "./ProductList.mjs";
 
@@ -19,3 +30,5 @@ const productListing = new ProductListing("your-categor", productDataUrl, listEl
 
 // Initialize the product listing to fetch and display products
 productListing.init();
+
+*/
