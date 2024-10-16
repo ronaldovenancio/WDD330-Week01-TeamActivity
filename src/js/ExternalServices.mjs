bad4c3620fa971b2986ef2f5b3fb6a8e65fe5459
ronaldovenancio <145623392+ronaldovenancio@users.noreply.github.com>
2024-10-16 11:32:50 -0300

    Create ExternalServices.mjs

---
+++ src/js/ExternalServices.mjs
@@ -0,0 +1,95 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+export default class ExternalServices {
+  constructor(category) {
+  //this.category = category;
+  //this.path = `../json/${this.category}.json`;
+  }
+
+  async getData(category) {
+    const response = await fetch(`${baseURL}products/search/${category}`);
+    const data = await convertToJson(response);
+    // console.log(data.Result);
+    return data.Result;
+  }
+
+  async findProductById(id) {
+    const response = await fetch(`${baseURL}product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+
+  async searchByTerm(term) {
+    const response = await fetch(`${baseURL}products/search?term=${term}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + "checkout/", options).then(convertToJson);
+  }
+
+}
+
+/*
+export default class ProductData {
+  constructor(category) {
+    this.category = category;
+    this.path = `../json/${this.category}.json`;
+  }
+  getData() {
+    return fetch(this.path)
+      .then(convertToJson)
+      .then((data) => data);
+  }
+  async findProductById(id) {
+    const products = await this.getData();
+    //console.log(products);
+    return products.find((item) => item.Id === id);
+  }
+}
+
+export async function findProductById(id) {
+  const products = await this.getData();
+  return products.find((item) => item.Id === id);
+} 
+*/
+
+
+
+/*
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+export async function findProductById(id) {
+  const products = await getData();
+  return products.find((item) => item.Id === id);
+} 
+
+
+export function getData(category = "tents") {
+  return fetch(`../json/${category}.json`)
+    .then(convertToJson)
+    .then((data) => data);
+}
+*/
