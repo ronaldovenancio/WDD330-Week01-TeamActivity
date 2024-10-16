8479a0d679e76945ccd0292a9b51114275c59098
ronaldovenancio <145623392+ronaldovenancio@users.noreply.github.com>
2024-10-16 11:37:52 -0300

    Update checkout.js

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,18 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
 
 loadHeaderFooter();
+
+const myCheckout = new CheckoutProcess("so-cart");
+myCheckout.init();
+
+document
+  .querySelector("#zip")
+  .addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
+
+// listening for click on the button
+document.querySelector("#paySubmit").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  myCheckout.checkout();
+});
