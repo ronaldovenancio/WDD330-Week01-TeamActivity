e590a62c55dce8a3659bf918e7f59e6381f35d29
ronaldovenancio <145623392+ronaldovenancio@users.noreply.github.com>
2024-10-16 11:36:35 -0300

    Update ShoppingCart.mjs

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,32 +1,294 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, cartSuperscript } from "./utils.mjs";
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+    return `
+          <li class="cart-card divider">
+              <a href="#" class="cart-card__image">
+                  <img
+                  src="${item.Images.PrimarySmall}"
+                  alt="${item.Name}"
+                  />
+              </a>
+              <a href="#">
+                  <h2 class="card__name">${item.Name}</h2>
+              </a>
+              <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+              <div class="qtd-container">
+                  <div class="qtd-button" id="qtdUp" data-id="${item.Id}">+</div>
+                  <p class="cart-card__quantity">qty: ${item.Qtd}</p>
+                  <div class="qtd-button" id="qtdDown" data-id="${item.Id}">-</div>
+              </div>
+              <p class="cart-card__price">$${item.FinalPrice.toFixed(2)}</p>
+              <button class="cart-card__remove" data-id="${item.Id}">X</button>
+          </li>
+      `;
+  }
+  
 
-  return newItem;
+// function cartItemTemplate(item) {
+//     const newItem = `<li class="cart-card divider">
+//     <a href="#" class="cart-card__image">
+//       <img
+//         src="${item.Images.PrimaryMedium}"
+//         alt="${item.Name}"
+//       > 
+//     </a>
+//     <a href="#">
+//       <h2 class="card__name">${item.Name}</h2>
+//     </a>
+//     <p class="cart-card__color">${item.Colors[0].ColorName}</p> 
+//     <div class="qty-input">
+//       <button class="minusBtn" data-id=${item.Id} type="button">-</button>
+//       <input class="product-qty" type="number" name="product-qty" min="0" max="10" value="1">
+//       <button class="addBtn" data-id=${item.Id} type="button">+</button>
+//   </div>
+//     <p class="cart-card__price">$${item.FinalPrice}</p>
+//     <span class="remove" data-id="${item.Id}">X<span>
+  
+//   </li>`;
+
+//     return newItem;
+// }
+
+
+export default class ShoppingCart {
+    constructor() {}
+  
+    // Renders the shopping cart contents
+    renderCartContents() {
+      // Get cart contents from localStorage if any
+      const cartItems = getLocalStorage("so-cart") || [];
+  
+      // If there are no items, we can stop here or show a message
+      if (cartItems.length === 0) {
+        document.querySelector(".product-list").innerHTML =
+          "<p>Your cart is empty.</p>";
+        this.cartSubtotal(cartItems);
+        return;
+      }
+      // If there are items, we can render them
+      const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+      document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  
+      // Set the total price of the cart
+      this.cartSubtotal(cartItems);
+  
+      // Create events listeners for removing items from the card
+      const removeButtons = document.querySelectorAll(".cart-card__remove");
+      removeButtons.forEach((button) => {
+        button.addEventListener("click", (event) => this.removeCartItem(event));
+      });
+  
+      // Create event listeners for qtd down button
+      const qtdDownButton = document.querySelectorAll("#qtdDown");
+      qtdDownButton.forEach((button) => {
+        button.addEventListener("click", (event) => this.decreaseQtdBy1(event));
+      });
+  
+      // Create event listeners for qtd up button
+      const qtdUpButton = document.querySelectorAll("#qtdUp");
+      qtdUpButton.forEach((button) => {
+        button.addEventListener("click", (event) => this.increaseQtdBy1(event));
+      });
+    }
+  
+    // Decreases qtd by 1
+    decreaseQtdBy1(event) {
+      const itemId = event.target.getAttribute("data-id");
+      const cartItems = getLocalStorage("so-cart");
+  
+      // Get index of item with matching id
+      const itemIndex = cartItems.findIndex((item) => item.Id === itemId);
+  
+      // Check if qtd is 1, remove item from cart if so
+      if (cartItems[itemIndex].Qtd === 1) {
+        this.removeCartItem(event);
+      } else {
+        // Decrease item qtd by 1
+        cartItems[itemIndex].Qtd -= 1;
+        cartItems[itemIndex].FinalPrice -= cartItems[itemIndex].ListPrice;
+  
+        // Update cart in localStorage with new qtd
+        setLocalStorage("so-cart", cartItems);
+  
+        // Rerender cart items
+        this.renderCartContents();
+      }
+    }
+  
+    // Increases qtd by 1
+    increaseQtdBy1(event) {
+      const itemId = event.target.getAttribute("data-id");
+      const cartItems = getLocalStorage("so-cart");
+  
+      // Get index of item with matching id
+      const itemIndex = cartItems.findIndex((item) => item.Id === itemId);
+  
+      // Increase item qtd by 1
+      cartItems[itemIndex].Qtd += 1;
+      cartItems[itemIndex].FinalPrice += cartItems[itemIndex].ListPrice;
+  
+      // Update cart in localStorage with new qtd
+      setLocalStorage("so-cart", cartItems);
+  
+      // Rerender cart items
+      this.renderCartContents();
+    }
+  
+    // Removes an item from the cart
+    removeCartItem(event) {
+      const itemId = event.target.getAttribute("data-id");
+      const cartItems = getLocalStorage("so-cart");
+  
+      const cartItemRemoved = cartItems.filter((item) => item.Id !== itemId);
+      setLocalStorage("so-cart", cartItemRemoved);
+  
+      // Load cartSuperscript
+      cartSuperscript();
+  
+      // Rerender shopping cart
+      this.renderCartContents();
+    }
+  
+    // Calculates the cart subtotal
+    cartSubtotal(items) {
+      const cartCard = document.querySelector(".cart-card__subtotal");
+      if (items.length <= 0) {
+        cartCard.classList.add("hide"); // Hide the cart subtotal
+      } else {
+        cartCard.classList.remove("hide"); // Show the cart subtotal
+  
+        const subtotal = items.reduce((acc, item) => acc + item.FinalPrice, 0);
+        // console.log({ items, subtotal });
+        const cartCount = items.reduce((acc, item) => acc + item.Qtd, 0);
+        // cart count
+        if (cartCount > 1) {
+          document.querySelector(".cart-count").textContent =
+            `${cartCount} items`;
+        } else {
+          document.querySelector(".cart-count").textContent = `${cartCount} item`;
+        }
+  
+        // cart subtotal
+        document.querySelector(".cart-subtotal").textContent =
+          ` $${subtotal.toFixed(2)}`;
+      }
+    }
+  }
+
+
+
+
+// daqui em diante irei rever
+/*
+function removeFromCart(targetElement) {
+    const clickedElement = targetElement.dataset.id;
+    const cart = getLocalStorage("cart");
+    var filtered = cart.filter(function (el) {
+        return el.Id != clickedElement;
+    });
+    setLocalStorage("cart", filtered);
+    renderCartContents();
+    totalCart();
+    updateCartCount();
+}
+
+function addQuantity(targetElement) {
+    const cart = getLocalStorage("cart")
+    const clickedItem = targetElement.dataset.id;
+    var inputQuantity = targetElement.previousElementSibling;
+    var add = parseInt(inputQuantity.value) + 1;
+    inputQuantity.value = add;
+
+    var itemLocated = cart.findIndex(obj => obj.Id == clickedItem);
+    cart[itemLocated].quantity = add;
+
+    setLocalStorage("cart", cart);
+    totalCart();
+}
+
+function minusQuantity(targetElement) {
+    const cart = getLocalStorage("cart")
+    const clickedItem = targetElement.dataset.id;
+    var quantity = targetElement.nextElementSibling;
+    var minus = parseInt(quantity.value);
+    minus -= 1;
+
+    if (minus <= 0) {
+        minus = 1;
+    }
+    quantity.value = minus;
+
+    var itemLocated = cart.findIndex(obj => obj.Id == clickedItem);
+    cart[itemLocated].quantity = minus;
+
+    setLocalStorage("cart", cart);
+    totalCart();
+}
+
+
+function totalCart() {
+    var items = getLocalStorage("cart");
+    var total = 0.0;
+
+    const cartFooter = document.querySelector(".cart-footer");
+    const cartTotal = document.querySelector(".cart-total");
+    if (items.length != 0) {
+        items.forEach((item) => {
+            total += parseFloat(item.FinalPrice) * item.quantity;
+        });
+
+        cartTotal.textContent = `total: $${total.toFixed(2)}`;
+        cartFooter.removeAttribute("hidden");
+    }
+
+    if (items.length == 0) {
+        cartFooter.hidden = true;
+    }
+}
+
+
+
+function renderCartContents() {
+    const cartItems = getLocalStorage("cart");
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+    var remove = document.getElementsByClassName("remove");
+    var add = document.getElementsByClassName("addBtn");
+    var minus = document.getElementsByClassName("minusBtn");
+
+    Array.from(remove).forEach(function (element) {
+        element.addEventListener("click", function (event) {
+            const targetElement = event.target;
+            removeFromCart(targetElement);
+        });
+    });
+
+    Array.from(add).forEach(function (element) {
+        element.addEventListener("click", function (event) {
+            const targetElement = event.target;
+            addQuantity(targetElement);
+        });
+    });
+
+    Array.from(minus).forEach(function (element) {
+        element.addEventListener("click", function (event) {
+            const targetElement = event.target;
+            minusQuantity(targetElement);
+        });
+    });
 }
 
 export default class ShoppingCart {
-  constructor(key, parentSelector) {
-    this.key = key;
-    this.parentSelector = parentSelector;
-  }
-  renderCartContents() {
-    const cartItems = getLocalStorage(this.key);
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
-  }
+    constructor(key, parentSelector) {
+        this.key = key;
+        this.parentSelector = parentSelector;
+    }
+
+    displaycart() {
+        renderCartContents();
+        totalCart();
+    }
 }
+*/
